02fd30327b0bbbb1a9b171f18aa7010599adb158
Nelson/Roberto <dewittn@gmail.com>
2017-08-21 08:58:58 -0400

    Moved “Books” HTML from post into theme. Removed extra css Added logic to only display Books section on certain posts

--- page_blog.php
+++ page_blog.php
@@ -16,15 +16,66 @@ get_header(); ?>
 
 				<div class="content-wrapper">
 					<h1 class="bigheading" style="max-width: 90%; margin: 0 auto 40px;"><?php the_title(); ?></h1>
-					<?php 
+					<?php
                         $content = get_the_content();                                        
                         $content = str_replace( ']]>', ']]&gt;', apply_filters( 'the_content', $content ) ); 
                         echo $content;
 					?>
-				</div>
+				</div><!-- end content-wrapper -->
 
 			<?php endwhile; endif; wp_reset_query(); ?>	
-
+            
+            <?php if ( 610 == $post->ID || 247 == $post->ID ) : ?>
+				<h2 style="text-align: center;">Books</h2>
+                <div class="divider"></div>
+                <div class="highroll">
+                    <div class="post leftpost">
+                        <a href="https://www.nelsonroberto.com/portfolio/innocent-dreams/" class="dropanchor"></a>
+                        <div class="post-snip">
+                            <div class="slip-line">
+                                <h2>Waking From Innocent Dreams</h2>
+                                <a href="https://www.nelsonroberto.com/portfolio/innocent-dreams/" class="std">
+                                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                                </a>
+                
+                                <p class="blurb-text">The Mausoleum Club stands on the quietest corner of the best residential street in the City. It is a Grecian building of white stone. About it are great elm trees with birds--the most expensive kind of birds--singing in the branches. The street in the softer hours of the morning has an almost reverential quiet. Great...</p>
+                            </div><!-- end slip-line -->
+                            <a href="https://www.nelsonroberto.com/portfolio/innocent-dreams/" class="std ion-ios-arrow-thin-right"></a>
+                        </div><!-- end post-snip -->
+                
+                        <div class="post-bg" style="background-image: url(https://www.nelsonroberto.com/wp-content/uploads/2017/07/Waking-From-Innocent-Dreams-Cover.png);">
+                            <div class="wager">
+                                <h4>In Development</h4>
+                            </div><!-- end wager -->
+                        </div><!-- end post-bg -->
+                    </div><!-- end post -->
+                
+                    <div class="post rightpost">
+                        <a href="https://www.nelsonroberto.com/portfolio/ksg/" class="dropanchor"></a>
+                
+                        <div class="post-snip">
+                            <div class="slip-line">
+                                <h2>A Kickstarter's Guide to Kickstarter</h2>
+                                <a href="https://www.nelsonroberto.com/portfolio/ksg/" class="std">
+                                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                                </a>
+                
+                                <p class="blurb-text">A Kickstarter’s Guide will give you a thorough understanding of the Kickstarter site, its functionality, practical usage, audience, and strategy. If you have never put together a campaign before or are having trouble getting traction, it will provide a framework to help you navigate the vague world of crowd-sourced funding.</p>
+                            </div><!-- end slip-line -->
+                            <a href="https://www.nelsonroberto.com/portfolio/ksg/" class="std ion-ios-arrow-thin-right"></a>
+                        </div><!-- end post-snip -->
+                
+                        <div class="post-bg" style="background-image: url(https://www.nelsonroberto.com/wp-content/uploads/2017/08/A-Kickstarters-Guide.jpg);">
+                            <div class="wager">
+                                <h4>2011</h4>
+                            </div><!-- end wager -->
+                        </div><!-- end post-bg -->
+                    </div><!-- end post -->
+                
+                    <div class="clear"></div>
+                </div><!-- end highroll -->
+                <h2 style="text-align: center;">Essays &amp; Articles</h2>
+            <?php endif; ?>
 		</div>
 
 		<div class="divider"></div>
